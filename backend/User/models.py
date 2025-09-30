dae8e6200608af7e30e193ae7f9270ca742c8738
Solomon Ador-Dionisio <solomon.adord@gmail.com>
2025-09-30 17:09:59 -0400

    okay

--- backend/User/models.py
+++ backend/User/models.py
@@ -5,6 +5,5 @@ class User(models.Model):
     id = models.BigAutoField(primary_key=True)
     full_name = models.CharField(max_length=50)
     given_name = models.CharField(max_length=50)
-    family_name = models.CharField(max_length=50)
     unique_id = models.CharField(max_length=500)
     email = models.CharField(max_length=50)
